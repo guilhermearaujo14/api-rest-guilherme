6760ea97ab83ed1ca761f6229423c63f5507bbf1
Guilherme Araujo <40188385+guilhermearaujo14@users.noreply.github.com>
2022-10-25 22:18:58 -0300

    Add files via upload

---
+++ apirest/src/test/java/com/produtos/apirest/ApirestApplicationTest.java
@@ -0,0 +1,14 @@
+package com.produtos.apirest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ApirestApplicationTest {
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ apirest/src/test/java/com/produtos/apirest/ApirestApplicationTestTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.produtos.apirest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Guilherme
+ *
+ */
+class ApirestApplicationTestTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
